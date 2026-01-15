8bc447a99a7609a0c7bdcb216cc91b3828cd1e48
Matheus Felippe Petry <matheus.petry@digitro.com>
2026-01-15 09:37:52 -0300

    Create footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Container, createTheme, ThemeProvider } from "@mui/material";
 import { Header } from "./components/Header";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Main } from "./components/Main";
+import { Rodape } from "./components/Rodape";
 
 const theme = createTheme({
   palette: {
@@ -19,9 +20,10 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Container>
+      <Container sx={{ display: "flex", flexDirection: "column", minHeight: '100vh' }}>
         <Header />
         <Main />
+        <Rodape />
       </Container>
     </ThemeProvider>
   );
--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,2 +1,10 @@
+import { Box, Typography } from "@mui/material"
 
 
+export const Rodape = () => {
+    return (
+        <Box component='footer' sx={{ borderTop: 1, textAlign: 'center', p: 5, mt: 'auto' }}>
+            <Typography>&copy; Desenvolvido por <strong>Matheus</strong></Typography>
+        </Box>
+    )
+}
